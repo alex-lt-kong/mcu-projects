bf284acc0db254b456f4a37fb7614b8201f96dae
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-08-02 16:40:57 +0800

    new projects

---
+++ esp32/temp-humidity-publisher/configs-sample.h
@@ -0,0 +1,8 @@
+#define WIFI_SSID "YourNetworkName"
+#define WIFI_PASSWORD "YourSecretPassword"
+
+#define MQTT_SERVER "mqtt.example.org"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "/mqtt/topic"
+#define MQTT_USERNAME "user"
+#define MQTT_PASSWORD "pa$$w0rd"
