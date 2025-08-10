f7b5d51aef9c831673619dc7c267e44a490e48a6
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-08-11 00:01:03 +0800

    LD2410 operational

---
+++ pico-w/ld2410-mqtts-publisher/configs-sample.h
@@ -0,0 +1,17 @@
+#define WIFI_SSID "YourNetworkName"
+#define WIFI_PASSWORD "YourSecretPassword"
+
+#define MQTT_CLIENT_ID "MyMqttClient"
+#define MQTT_SERVER "mqtt.example.org"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "/mqtt/topic"
+#define MQTT_USERNAME "user"
+#define MQTT_PASSWORD "pa$$w0rd"
+
+const char* root_ca = R"(
+-----BEGIN CERTIFICATE-----
+MIIDijCCAxGgAwIBAgISBTSvSClNntOf2PNprdOvDR1aMAoGCCqGSM49BAMDMDIx
+...
+zrzAmJMVsynGIOckYCzc3VS9tfRUFnsfLUatPWNSfUVt2aSHKS8SfN0GVBt5sw==
+-----END CERTIFICATE-----
+)";
--- pico-w/ld2410-mqtts-publisher/ld2410-mqtts-publisher.ino
+++ pico-w/ld2410-mqtts-publisher/ld2410-mqtts-publisher.ino
@@ -1,3 +1,4 @@
+#include "configs.h"
 #define sensorSerial Serial1
 
 
@@ -8,7 +9,11 @@
 
 //Change the communication baud rate here, if necessary
 //#define LD2410_BAUD_RATE 256000
-#include "MyLD2410.h"
+#include <MyLD2410.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+#include <Wire.h>
 
 #ifdef DEBUG_MODE
 MyLD2410 sensor(sensorSerial, true);
@@ -16,7 +21,7 @@ MyLD2410 sensor(sensorSerial, true);
 MyLD2410 sensor(sensorSerial);
 #endif
 
-unsigned long nextPrint = 0, printEvery = 1000;  // print every second
+unsigned long nextPrint = 0, interval_ms = 5000; 
 
 void printValue(const byte &val) {
   Serial.print(' ');
@@ -62,7 +67,7 @@ void printData() {
     Serial.println();
   }
 
-  if (sensor.inEnhancedMode() && (sensor.getFirmwareMajor() > 1)) { 
+  if (sensor.inEnhancedMode() && (sensor.getFirmwareMajor() > 1)) {
     Serial.print("Light level: ");
     Serial.println(sensor.getLightLevel());
     Serial.print("Output level: ");
@@ -72,10 +77,55 @@ void printData() {
   Serial.println();
 }
 
+WiFiClientSecure wifi_client;
+PubSubClient mqtts_client(wifi_client);
+
+void setup_wifi(int attempts = 0) {
+  const auto max_attempts = 10;
+  if (attempts >= max_attempts) {
+    Serial.printf("\nmax_attempts (%d) reached, restarting the device as the last resort\n", max_attempts);
+#if defined(ARDUINO_ARCH_ESP32)
+    ESP.restart();
+#elif defined(ARDUINO_ARCH_RP2040)
+    rp2040.reboot();
+#endif
+  }
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.printf("\nConnecting to WiFi [" WIFI_SSID "] (%d/%d)\n", attempts + 1, max_attempts);
+
+  for (int i = 0; i < 60; ++i) {
+    delay(1000);
+    Serial.print(".");
+    if (WiFi.status() != WL_CONNECTED) continue;
+
+    Serial.println("\nWiFi connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    return;
+  }
+  setup_wifi(attempts + 1);
+}
+
+void reconnect_mqtts() {
+  while (!mqtts_client.connected()) {
+    Serial.printf("Connecting to MQTT broker " MQTT_SERVER ":%d\n", MQTT_PORT);
+    if (mqtts_client.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) {
+      Serial.println("connected");
+    } else {
+      const int retry_delay_ms = 10 * 1000;
+      Serial.print("mqtts_client.connect() failed, mqtts_client.state() = ");
+      Serial.print(mqtts_client.state());
+      Serial.printf(" retrying in %d seconds\n", retry_delay_ms / 1000);
+      delay(retry_delay_ms);
+    }
+  }
+}
+
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
-  sensorSerial.setTX(0); // GP0
-  sensorSerial.setRX(1); // GP1
+  sensorSerial.setTX(0);  // GP0
+  sensorSerial.setRX(1);  // GP1
   sensorSerial.begin(LD2410_BAUD_RATE);
 
 
@@ -93,11 +143,28 @@ void setup() {
 #endif
 
   delay(nextPrint);
+  setup_wifi();
+  wifi_client.setCACert(root_ca);
+  mqtts_client.setServer(MQTT_SERVER, MQTT_PORT);
 }
 
 void loop() {
+  //delay(5000);
   if ((sensor.check() == MyLD2410::Response::DATA) && (millis() > nextPrint)) {
-    nextPrint = millis() + printEvery;
-    printData();
+    nextPrint = millis() + interval_ms;
+    return;
+    
   }
+  String payload = "{";
+    payload += "\"motion_state\": " + String((sensor.getOutLevel()) ? "1" : "0");
+    payload += "}";
+
+
+    if (!mqtts_client.connected()) {
+      reconnect_mqtts();
+    }
+
+    Serial.printf("Publishing to topic %s: ", MQTT_TOPIC);
+    Serial.println(payload);
+    mqtts_client.publish(MQTT_TOPIC, payload.c_str());
 }
